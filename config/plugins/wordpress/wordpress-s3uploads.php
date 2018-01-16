4e7b048e0b3460465055069dbd59d42dd1e8767c
Diego García <diego.garcia.segura@gmail.com>
2018-01-16 16:08:59 -0600

    Changes to upload files to AWS S2, 6th try

--- config/plugins/wordpress/wordpress-s3uploads.php
+++ config/plugins/wordpress/wordpress-s3uploads.php
@@ -6,7 +6,8 @@
 if (!empty(getenv('AWS_S3_URL'))) {
     $env = parse_url(getenv('AWS_S3_URL'));
 
-    error_log("hello Diego, this is a test!" + ' ' + $env['host']);
+    error_log("hello Diego, this is a test!");
+    error_log($env['host']);
 
     define('S3_UPLOADS_AUTOENABLE', true);
     define('S3_UPLOADS_KEY', $env['user']);
