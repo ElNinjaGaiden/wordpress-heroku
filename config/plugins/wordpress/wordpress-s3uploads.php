919e7b04938b992c9bfa7536f0902c69853ba078
Diego García <diego.garcia.segura@gmail.com>
2018-01-16 16:25:35 -0600

    Changes to upload files to AWS S2, final code

--- config/plugins/wordpress/wordpress-s3uploads.php
+++ config/plugins/wordpress/wordpress-s3uploads.php
@@ -12,12 +12,6 @@ if (!empty(getenv('AWS_S3_URL'))) {
     define('S3_UPLOADS_REGION', str_replace(array('s3-', '.amazonaws.com'), array('', ''), $env['host']));
     define('S3_UPLOADS_BUCKET', ltrim($env['path'], '/'));
 
-    // define('S3_UPLOADS_AUTOENABLE', true);
-    // define('S3_UPLOADS_BUCKET', 'dgarciasblogbucket');
-    // define('S3_UPLOADS_KEY', 'AKIAJLGCNZCERWD5T3ZA');
-    // define('S3_UPLOADS_SECRET', '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');
-    // define('S3_UPLOADS_REGION', 'us-east-2');
-
     unset($env);
 } else {
     define('S3_UPLOADS_AUTOENABLE', false);
